bb93990203b343e757857829befdc3ff3f0a6ec7
D2rkL1fe <d2rkl1fe@gmail.com>
2025-10-25 02:52:00 +0300

    Refactoring of Refactored code

--- scenes/farm/farm.gd
+++ scenes/farm/farm.gd
@@ -1,46 +1,15 @@
-extends Control
-class_name FarmUISettings
+extends Node2D
+class_name Farm
 
-signal reset_camera_view_signal
-signal focus_pet_signal(target: Entity)
+@export var spawn : Node2D
 
-@onready var pet_info_panel: PanelContainer = $PetInfoPanel
-@onready var pet_name_label: Label = $PetInfoPanel/VBoxContainer.find_child("NameLabel", false)
-@onready var hunger_progress: ProgressBar = $PetInfoPanel/VBoxContainer.find_child("HungerBar", false)
-@onready var pet_love_label: Label = $PetInfoPanel/VBoxContainer.find_child("LoveLabel", false)
+func _ready() -> void:
+	spawn_pets()
 
-var focused_pet: Entity = null
-
-func _ready():
-	pet_info_panel.hide()
-	
-	var manager = get_tree().get_first_node_in_group("ExpeditionManager")
-	if manager:
-		manager.pet_interacted.connect(open_pet_interaction_menu)
-
-func update_ui_display() -> void:
-	if focused_pet:
-		if pet_name_label:
-			pet_name_label.text = focused_pet.data.entity_name
-		if hunger_progress:
-			hunger_progress.value = focused_pet.data.hunger
-		if pet_love_label:
-			pet_love_label.text = "Love: %d" % focused_pet.data.love
-
-func open_pet_interaction_menu(pet: Entity) -> void:
-	focused_pet = pet
-	focus_pet_signal.emit(pet)
-	update_ui_display()
-	pet_info_panel.show()
-
-func _on_feed_button_pressed() -> void:
-	if focused_pet:
-		focused_pet.data.hunger = min(100, focused_pet.data.hunger + 15)
-		focused_pet.data.love = min(999, focused_pet.data.love + 1)
-		update_ui_display()
-		print(focused_pet.data.entity_name + " was fed!")
-
-func _on_close_button_pressed() -> void:
-	focused_pet = null
-	pet_info_panel.hide()
-	reset_camera_view_signal.emit()
+func spawn_pets():
+	for pet in Stats.pets:
+		var instance = PetLoader.pets[pet.entity_name].instantiate()
+		
+		instance.global_position = Vector2(randf_range(-50, 50), randf_range(-50, 50))
+		
+		spawn.add_child(instance)
--- scenes/farm/farm_ui.gd
+++ scenes/farm/farm_ui.gd
@@ -1,46 +1,31 @@
 extends Control
-class_name FarmUISettings
 
-signal reset_camera_view_signal
-signal focus_pet_signal(target: Entity)
+@export var camera : Camera2D
+@export var info : Control
 
-@onready var pet_info_panel: PanelContainer = $PetInfoPanel
-@onready var pet_name_label: Label = $PetInfoPanel/VBoxContainer.find_child("NameLabel", false)
-@onready var hunger_progress: ProgressBar = $PetInfoPanel/VBoxContainer.find_child("HungerBar", false)
-@onready var pet_love_label: Label = $PetInfoPanel/VBoxContainer.find_child("LoveLabel", false)
+var target : Node2D
 
-var focused_pet: Entity = null
+func _ready() -> void:
+	Global.pet_selected.connect(_on_pet_selected)
 
-func _ready():
-	pet_info_panel.hide()
+func _physics_process(delta: float) -> void:
+	# input
+	if Input.is_action_just_pressed("cancel"):
+		target = null
+		info.visible = false
 	
-	var manager = get_tree().get_first_node_in_group("ExpeditionManager")
-	if manager:
-		manager.pet_interacted.connect(open_pet_interaction_menu)
+	# animations
+	if target:
+		camera.zoom = lerp(camera.zoom, Vector2(2.0, 2.0), 8.0 * delta)
+		camera.global_position = lerp(camera.global_position, target.global_position + Vector2(18.0, 0), 8.0 * delta)
+	else:
+		camera.zoom = lerp(camera.zoom, Vector2.ONE, 8.0 * delta)
+		camera.global_position = lerp(camera.global_position, Vector2.ZERO, 8.0 * delta)
 
-func update_ui_display() -> void:
-	if focused_pet:
-		if pet_name_label:
-			pet_name_label.text = focused_pet.data.entity_name
-		if hunger_progress:
-			hunger_progress.value = focused_pet.data.hunger
-		if pet_love_label:
-			pet_love_label.text = "Love: %d" % focused_pet.data.love
+func reset_camera_view() -> void:
+	target = null
+	info.visible = false
 
-func open_pet_interaction_menu(pet: Entity) -> void:
-	focused_pet = pet
-	focus_pet_signal.emit(pet)
-	update_ui_display()
-	pet_info_panel.show()
-
-func _on_feed_button_pressed() -> void:
-	if focused_pet:
-		focused_pet.data.hunger = min(100, focused_pet.data.hunger + 15)
-		focused_pet.data.love = min(999, focused_pet.data.love + 1)
-		update_ui_display()
-		print(focused_pet.data.entity_name + " was fed!")
-
-func _on_close_button_pressed() -> void:
-	focused_pet = null
-	pet_info_panel.hide()
-	reset_camera_view_signal.emit()
+func _on_pet_selected(pet):
+	target = pet
+	info.visible = true
